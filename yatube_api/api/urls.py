62a73c92371029fc84ea5e01ac313afef557d016
Долгушин Антон <antodolg@gmail.com>
2023-01-13 15:50:31 +0800

    на первую проверку

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, GroupViewSet, PostViewSet
+
+
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='post')
+router_v1.register('groups', GroupViewSet, basename='group')
+router_v1.register(r'posts/(?P<post_id>\d+)/comments',
+                   CommentViewSet,
+                   basename='comment')
+
+urlpatterns = [
+    path('v1/api-token-auth/', obtain_auth_token),
+    path('v1/', include(router_v1.urls)),
+]
---
+++ yatube_api/api/views.py
@@ -0,0 +1,59 @@
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
+from .serializers import (
+    CommentSerializer, GroupSerializer, PostSerializer
+)
+from posts.models import Group, Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            Response(status=status.HTTP_403_FORBIDDEN)
+            raise PermissionDenied('Изменение чужого поста запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            Response(status=status.HTTP_403_FORBIDDEN)
+            raise PermissionDenied('Удаление чужого поста запрещено!')
+        super(PostViewSet, self).perform_destroy(serializer)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
+    def perform_create(self, serializer):
+        serializer.save(post=self.get_post(), author=self.request.user)
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            Response(status=status.HTTP_403_FORBIDDEN)
+            raise PermissionDenied('Изменение чужого коментария запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            Response(status=status.HTTP_403_FORBIDDEN)
+            raise PermissionDenied('Удаление чужого коментария запрещено!')
+        super(CommentViewSet, self).perform_destroy(serializer)
